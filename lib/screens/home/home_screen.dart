2b8c596d7498001b35ca867de486e33b665a4773
Jeremy Chandler <j0c0erv@HOMEOFFICE.wal-mart.com>
2020-10-15 20:34:20 -0500

    No commit message

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -48,7 +48,6 @@ class HomeScreen extends StatelessWidget {
           showModalBottomSheet(
               context: context,
               builder: (context) => Container(
-                    height: 200,
                     color: Colors.white,
                     child: AddMealForm(),
                   )),
@@ -104,6 +103,25 @@ class _AddMealFormState extends State<AddMealForm> {
               children: <Widget>[
                 FoodNameInput(),
                 Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(55.0),
+                        ),
+                        fillColor: Colors.grey.shade300,
+                        filled: true,
+                        labelText: 'Food Name',
+                        contentPadding: EdgeInsets.only(left: 30)),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
                   padding: const EdgeInsets.symmetric(vertical: 16.0),
                   child: ElevatedButton(
                     onPressed: () {
@@ -137,21 +155,24 @@ class FoodNameInput extends StatefulWidget {
 class _FoodNameInputState extends State<FoodNameInput> {
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      decoration: InputDecoration(
-          border: OutlineInputBorder(
-            borderRadius: new BorderRadius.circular(55.0),
-          ),
-          fillColor: Colors.grey.shade300,
-          filled: true,
-          labelText: 'Food Name',
-          contentPadding: EdgeInsets.only(left: 30)),
-      validator: (value) {
-        if (value.isEmpty) {
-          return 'Please enter some text';
-        }
-        return null;
-      },
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: TextFormField(
+        decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: new BorderRadius.circular(55.0),
+            ),
+            fillColor: Colors.grey.shade300,
+            filled: true,
+            labelText: 'Food Name',
+            contentPadding: EdgeInsets.only(left: 30)),
+        validator: (value) {
+          if (value.isEmpty) {
+            return 'Please enter some text';
+          }
+          return null;
+        },
+      ),
     );
   }
 }
