bc162e1c2152185c71dc77d2a071fa008c9ab175
Jeremy Chandler <j0c0erv@HOMEOFFICE.wal-mart.com>
2020-10-15 20:27:39 -0500

    No commit message

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -102,22 +102,7 @@ class _AddMealFormState extends State<AddMealForm> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                TextFormField(
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: new BorderRadius.circular(55.0),
-                      ),
-                      fillColor: Colors.grey.shade300,
-                      filled: true,
-                      labelText: 'Food Name',
-                      contentPadding: EdgeInsets.only(left: 30)),
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Please enter some text';
-                    }
-                    return null;
-                  },
-                ),
+                FoodNameInput(),
                 Padding(
                   padding: const EdgeInsets.symmetric(vertical: 16.0),
                   child: ElevatedButton(
@@ -136,6 +121,37 @@ class _AddMealFormState extends State<AddMealForm> {
           ),
         ),
       ),
+    );
+  }
+}
+
+class FoodNameInput extends StatefulWidget {
+  const FoodNameInput({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _FoodNameInputState createState() => _FoodNameInputState();
+}
+
+class _FoodNameInputState extends State<FoodNameInput> {
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderRadius: new BorderRadius.circular(55.0),
+          ),
+          fillColor: Colors.grey.shade300,
+          filled: true,
+          labelText: 'Food Name',
+          contentPadding: EdgeInsets.only(left: 30)),
+      validator: (value) {
+        if (value.isEmpty) {
+          return 'Please enter some text';
+        }
+        return null;
+      },
     );
   }
 }
