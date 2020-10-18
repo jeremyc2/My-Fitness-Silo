c1d61be99a16204c0a3da6a0f693a1e7e4f55758
Jeremy Chandler <goldenmeso@gmail.com>
2020-10-17 20:16:56 -0500

    modules

--- lib/screens/home/modules/macro_input.dart
+++ lib/screens/home/modules/macro_input.dart
@@ -12,21 +12,24 @@ class MacroInput extends StatefulWidget {
 class _MacroInputState extends State<MacroInput> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: TextFormField(
-        decoration: InputDecoration(
-          border: OutlineInputBorder(),
-          fillColor: Colors.grey.shade300,
-          filled: true,
-          labelText: widget.name,
+    return SizedBox(
+      width: 100,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: TextFormField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            fillColor: Colors.grey.shade300,
+            filled: true,
+            labelText: widget.name,
+          ),
+          validator: (value) {
+            if (value.isEmpty) {
+              return 'Please enter some text';
+            }
+            return null;
+          },
         ),
-        validator: (value) {
-          if (value.isEmpty) {
-            return 'Please enter some text';
-          }
-          return null;
-        },
       ),
     );
   }
